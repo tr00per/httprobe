8d3ead2d5e2c6854a35afcceedb189df2c0c84ba
Artur Czajka <a.czajka@oberthur.com>
2017-10-19 12:03:42 +0000

    Added possibility to return different HTTP codes

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,11 @@ import           System.Environment (getArgs)
 
 main :: IO ()
 main = do
-    port:_ <- getArgs
-    putStrLn ("Running on " ++ port)
-    startProbe (read port )
+    (port, status) <- extractArgs =<< getArgs
+    putStrLn ("Running on " ++ port ++ ", will return HTTP " ++ status)
+    startProbe (read port) (read status)
+  where
+    extractArgs :: [String] -> IO (String, String)
+    extractArgs args = if length args /= 2
+        then error "Give 2 arguments: port and code. For example: 8080 200"
+        else let port:status:_ = args in return (port, status)
--- src/Network/HTTP/Probe.hs
+++ src/Network/HTTP/Probe.hs
@@ -6,21 +6,21 @@ module Network.HTTP.Probe (
 import           Data.ByteString.Char8    (unpack)
 import           Data.CaseInsensitive     (original)
 import           Data.Tuple.Extra         ((***))
-import           Network.HTTP.Types       (status200)
+import           Network.HTTP.Types       (Status)
 import           Network.Wai              (Application, rawPathInfo,
                                            requestBody, requestHeaders,
                                            responseLBS)
 import           Network.Wai.Handler.Warp (run)
 
-startProbe :: Int -> IO ()
-startProbe port = run port app
+startProbe :: Int -> Int -> IO ()
+startProbe port = run port . app
 
-app :: Application
-app request sendResponse = do
+app :: Int -> Application
+app status request sendResponse = do
     let headers = requestHeaders request
     body <- requestBody request
     putStrLn $ "---- endpoint: " ++ unpack (rawPathInfo request)
     mapM_ (print . (original *** unpack)) headers
     putStrLn $ unpack body
     putStrLn "^^^^^^^^^^^^\n"
-    sendResponse $ responseLBS status200 [] ""
+    sendResponse $ responseLBS (toEnum status) [] ""
