d5fd420f9a714a22644aa558cdb635d1204c4233
Artur Czajka <a.czajka@oberthur.com>
2019-01-03 14:03:05 +0000

    Added option to run probe with occasional error responses

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,20 @@
 module Main where
 
-import           Network.HTTP.Probe (startProbe)
+import           Network.HTTP.Probe (ProbeArgs (EchoProbe, ProbeWithErrors),
+                                     startProbe)
 import           System.Environment (getArgs)
 
 main :: IO ()
 main = do
-    (port, status) <- extractArgs =<< getArgs
-    putStrLn ("Running on " ++ port ++ ", will return HTTP " ++ status)
-    startProbe (read port) (read status)
+    pArgs <- extractArgs =<< getArgs
+    putStrLn $ "Running " ++ show pArgs
+    startProbe pArgs
   where
-    extractArgs :: [String] -> IO (String, String)
-    extractArgs args = if length args /= 2
-        then error "Give 2 arguments: port and code. For example: 8080 200"
-        else let port:status:_ = args in return (port, status)
+    extractArgs :: [String] -> IO ProbeArgs
+    extractArgs args = case args of
+            [port, success] ->
+                return $ EchoProbe (read port) (read success)
+            [port, successCode, errorCode, ratio] ->
+                return $ ProbeWithErrors (read port) (read successCode) (read errorCode) (read ratio)
+            _ ->
+                error "Pass 2 or 4 arguments: <port> <success code> [<error code> <error ratio>]\n\tport - listen port number\n\tsuccess code, error code - HTTP response codes\n\terror ratio - 0-100 - how often an error should be returned (on avg.)\n"
--- src/Network/HTTP/Probe.hs
+++ src/Network/HTTP/Probe.hs
@@ -1,26 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.Probe (
-    startProbe
+    ProbeArgs (..), startProbe
 ) where
 
 import           Data.ByteString.Char8    (unpack)
 import           Data.CaseInsensitive     (original)
 import           Data.Tuple.Extra         ((***))
-import           Network.HTTP.Types       (Status)
 import           Network.Wai              (Application, rawPathInfo,
                                            requestBody, requestHeaders,
                                            responseLBS)
 import           Network.Wai.Handler.Warp (run)
+import           System.Random
 
-startProbe :: Int -> Int -> IO ()
-startProbe port = run port . app
+data ProbeArgs = EchoProbe { port        :: Int
+                           , successCode :: Int
+                           }
+               | ProbeWithErrors { port        :: Int
+                                 , successCode :: Int
+                                 , errorCode   :: Int
+                                 , ratio       :: Int
+                                 }
+               deriving Show
 
-app :: Int -> Application
-app status request sendResponse = do
+startProbe :: ProbeArgs -> IO ()
+startProbe args = run (port args) . app $ args
+
+app :: ProbeArgs -> Application
+app args request sendResponse = do
     let headers = requestHeaders request
     body <- requestBody request
-    putStrLn $ "---- endpoint: " ++ unpack (rawPathInfo request)
+    putStrLn $ "> Request at " ++ unpack (rawPathInfo request)
     mapM_ (print . (original *** unpack)) headers
     putStrLn $ unpack body
-    putStrLn "^^^^^^^^^^^^\n"
+    putStrLn "^^^^^^^^^^^^"
+    status <- determineStatus args
+    putStrLn $ "< Response code: " ++ show status ++ "\n"
     sendResponse $ responseLBS (toEnum status) [] ""
+
+determineStatus :: ProbeArgs -> IO Int
+determineStatus pArgs@EchoProbe {} =
+    return $ successCode pArgs
+determineStatus pArgs@ProbeWithErrors {} = do
+    rand <- getStdRandom $ randomR (0, 99)
+    return $ if rand < ratio pArgs then errorCode pArgs else successCode pArgs
